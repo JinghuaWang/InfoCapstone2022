3006fe15b28c1a1827663c005671c3c6a3c09754
John Wang <johnjinghuawang@gmail.com>
2023-02-09 20:56:09 -0800

    change file structure

---
+++ src/.umi/core/routes.ts
@@ -0,0 +1,40 @@
+// @ts-nocheck
+import React from 'react';
+import { ApplyPluginsType } from '/Users/johnwang/development/InfoCapstone2022/INFOCAPSTONE/node_modules/umi/node_modules/@umijs/runtime';
+import * as umiExports from './umiExports';
+import { plugin } from './plugin';
+
+export function getRoutes() {
+  const routes = [
+  {
+    "path": "/",
+    "component": require('@/pages/homePage').default,
+    "exact": true
+  },
+  {
+    "path": "/courseInfo",
+    "component": require('@/pages/courseInfo').default,
+    "routes": [
+      {
+        "path": "/courseInfo/detail",
+        "component": require('@/pages/courseInfoPage').default,
+        "exact": true
+      },
+      {
+        "path": "/courseInfo/QA",
+        "component": require('@/pages/QAPage').default,
+        "exact": true
+      }
+    ]
+  }
+];
+
+  // allow user to extend routes
+  plugin.applyPlugins({
+    key: 'patchRoutes',
+    type: ApplyPluginsType.event,
+    args: { routes },
+  });
+
+  return routes;
+}
---
+++ src/.umi/plugin-helmet/exports.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// @ts-ignore
+export { Helmet } from '/Users/johnwang/development/InfoCapstone2022/INFOCAPSTONE/node_modules/react-helmet';
