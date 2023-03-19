6e91c1aa0423a5fe0c8538ab0a6152fece6e8378
John Wang <johnjinghuawang@gmail.com>
2023-03-19 14:25:22 -0700

    update Google Analytics tracking id

--- src/models/google_analystics.tsx
+++ src/models/google_analystics.tsx
@@ -1,6 +1,6 @@
 import ReactGA from 'react-ga4';
 
-const TRACKING_ID = "G-RPCDY8SNQ3"
+const TRACKING_ID = "G-1X4JT54S1M"
 ReactGA.initialize(TRACKING_ID);
 
 const useAnalyticsEventTracker = (category="Main Page", action = "test action", label = "test label") => {
